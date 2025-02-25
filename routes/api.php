8c2237bd86c0e4ba9ca603a8693a70f577a16f92
Ahmed Mansoor <ahmadh.mansoor@gmail.com>
2025-02-25 21:15:02 +0500

    public routes and route service provider

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\VehicleTypeController;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/vehicles', [VehicleController::class, 'index']);
+Route::get('/vehicles/{vehicle}', [VehicleController::class, 'show']);
+Route::get('/vehicle-types', [VehicleTypeController::class, 'index']);
+
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\VehicleController;
-use App\Http\Controllers\VehicleTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/vehicles', [VehicleController::class, 'index']);
-Route::get('/vehicles/{vehicle}', [VehicleController::class, 'show']);
-Route::get('/vehicle-types', [VehicleTypeController::class, 'index']);
