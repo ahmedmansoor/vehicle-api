a744db396a83b762c95ecac4f8b9166e7f993edd
Ahmed Mansoor <ahmadh.mansoor@gmail.com>
2025-02-25 21:08:58 +0500

    public routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\VehicleTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/vehicles', [VehicleController::class, 'index']);
+Route::get('/vehicles/{vehicle}', [VehicleController::class, 'show']);
+Route::get('/vehicle-types', [VehicleTypeController::class, 'index']);
